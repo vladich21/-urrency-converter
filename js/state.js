6c9b1b5df9d02f20b320767df6e043dd4e2480d4
Vladislav <vladkoval039@gmail.com>
2024-10-01 11:43:18 +0300

    first commit

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+	url:"https://v6.exchangerate-api.com/v6/3aafadd1e23fdf7e70d50843",
+	codes: [],
+  pair: {
+    from: "",
+    to: "",
+  },
+  amount: "",
+  loading: false,
+  currentTab: "convert",
+  currency: {
+    code: "USD",
+  },
+  currencies: ["USD", "EUR", "BYN"],
+  actions: {
+    remove: "remove",
+    change: "change",
+  },
+};
